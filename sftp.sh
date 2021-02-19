d0ffa450b51487b6892108bad6b955cac38c6b84
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2021-02-19 06:36:55 -0500

    Add files via upload

---
+++ sftp.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/expect
+sudo yum update -y
+sudo yum install keychain -y
+sudo yum install sshpass -y
+sudo yum install expect -y
+
+spawn sftp user@192.168.0.103
+expect "password:"
+send "yourpasswordhere\n"
+expect "sftp>"
+send "cd logdirectory\n"
+expect "sftp>"
+send "put /var/log/file.log\n"
+expect "sftp>"
+send "exit\n"
+interact
+
+export SSHPASS=your-password-here
+sshpass -e sftp -oBatchMode=no -b - sftp-user@remote-host << !
+   cd incoming
+   put your-log-file.log
+   bye
+!
+
+#lftp -e 'mirror -R /local/log/path/ /remote/path/' --env-password -u user sftp.foo.com
+lftp --env-password sftp://user@host  -e "put local-file.name; bye"
+
+
+vi test_script.sh
+#!/bin/sh
+HOST=<yourhostname>
+USER=<someusername>
+PASSWD=<yourpasswd>
+
+cd <base directory for your put file>
+
+lftp<<END_SCRIPT
+open sftp://$HOST
+user $USER $PASSWD
+put local-file.name
+bye
+END_SCRIPT
+
+chmod +x test_script.sh 
+./test_script.sh
+
+echo "
+  ascii
+  cd pub
+  lcd dir_name
+  put filename
+  close
+  quit
+    "
+) | sftp --password="password" "user"@"servername"
