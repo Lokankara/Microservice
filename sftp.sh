4a9a7418e5916bb569433e16f975fcee800f3030
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2021-02-23 17:17:52 +0200

    Update sftp.sh

--- sftp.sh
+++ sftp.sh
@@ -1,51 +1,27 @@
-#!/usr/bin/expect
-sudo yum update -y
-sudo yum install keychain -y
-sudo yum install sshpass -y
-sudo yum install expect -y
-
-spawn sftp user@192.168.0.103
-expect "password:"
-send "yourpasswordhere\n"
-expect "sftp>"
-send "cd logdirectory\n"
-expect "sftp>"
-send "put /var/log/file.log\n"
-expect "sftp>"
-send "exit\n"
-interact
-
-export SSHPASS=your-password-here
-sshpass -e sftp -oBatchMode=no -b - sftp-user@remote-host << !
-   cd incoming
-   put your-log-file.log
-   bye
-!
-
-#lftp -e 'mirror -R /local/log/path/ /remote/path/' --env-password -u user sftp.foo.com
-lftp --env-password sftp://user@host  -e "put local-file.name; bye"
-
 #!/bin/bash
+HOST="192.168.0.103"
+NAME="user"
+FILE1="/tmp/*.log"
+FILE2="log.txt"
+sudo apt-get install ftp
+command -v ftp
+sudo yum install ftp
+command -v ftp
+ftp $HOST
+ls
+get $FILE1
+put $FILE1
+ls /home/$NAME
 
-sftp -oPort=21 user@192.168.0.130:/home/user/test/update <<EOF
-put /home/user/.m2/repository/com/pasha/TestPlugin/0.0.1-SNAPSHOT/TestPlugin-0.0.1-SNAPSHOT.jar 
-exit
-EOF
+#sftp
+sftp $HOST
+#/home/user/.ssh/known_hosts by default
+scp $FILE1 $NAME@$HOST:$FILE2
+ls
+get $FILE1
+$ put $FILE1
+ls -l
+#scp user@192.168.178.28:file user@192.168.178.27:file
 
-vi test_script.sh
-#!/bin/sh
-HOST=<yourhostname>
-USER=<someusername>
-PASSWD=<yourpasswd>
-
-cd <base directory for your put file>
-
-lftp<<END_SCRIPT
-open sftp://$HOST
-user $USER $PASSWD
-put local-file.name
-bye
-END_SCRIPT
-
-chmod +x test_script.sh
-./test_script.sh
+sudo yum install rsync
+rsync $FILE1 $NAME@$HOST:$FILE2
